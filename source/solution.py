73160d18b8e01aa44ecd5ddcf6579108050143e3
Buberman <iambuberman@gmail.com>
2024-04-12 14:14:56 +0700

    Update solution.py

--- source/solution.py
+++ source/solution.py
@@ -1,4 +1,11 @@
 class KnowledgeBase:
     def __init__(self):
         self.clauses = []
-        
+    
+    def negativetify(self, atom):
+        if atom[0] == '-':
+            return atom[1:]
+        else:
+            return '-' + atom
+    
+    def
