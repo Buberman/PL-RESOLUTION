363598c8963813363056f9e923557764bb0a591e
Buberman <iambuberman@gmail.com>
2024-04-12 15:07:56 +0700

    Update solution.py

--- source/solution.py
+++ source/solution.py
@@ -1,4 +1,3 @@
-import itertools
 class KnowledgeBase:
     def __init__(self):
         self.clauses = []
@@ -17,22 +16,16 @@ class KnowledgeBase:
                 return True
         return False
     
-
-
-    def negativeQuery(self, query):
-        res = []
-        for clause in query:
-            new = []
-            for atom in clause:
-                new.append([set.netativetify(atom)])
-            res.append(new)
-        
-        if len(res) == 1:
-             return list(itertools.chain.from_iterable(res))
-        else:
-            return self.toCNF(res)
+    def addClause(self, clause):
+        if clause not in self.clauses and not self.checkcomp(clause):
+            self.clauses.append(clause)
     
-    def toCNF(self, clause):
+
+  
+    
+        
+    
+    
 
         
 
