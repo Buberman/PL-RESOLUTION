f5883ca89cbacdca7e00dbb5ac523abb71ab9640
Buberman <iambuberman@gmail.com>
2024-04-12 16:59:14 +0700

    add main file content

---
+++ source/main.py
@@ -0,0 +1,58 @@
+import os
+import solution
+
+# Set input and output directories to the current directory
+INPUT_DIR = './'
+OUTPUT_DIR = './'
+
+def readKB(filename):
+    content = []
+    with open(filename, 'r') as f:
+        # Skip the first line
+        next(f)
+        content = f.read().splitlines()
+
+    alpha_size = len(content)
+    query_string = content[:alpha_size]
+    query = []
+    for cnf in query_string:
+        clause = cnf.split()
+        clause = list(filter(lambda x: x != 'OR', clause))
+        query.append(clause)
+
+    KB = solution.KnowledgeBase()
+    KB_size = 0  # No need to read this value anymore
+    KB_string = content[alpha_size + 1:]
+    for cnf in KB_string:
+        clause = cnf.split()
+        clause = list(filter(lambda x: x != 'OR', clause))
+        KB.addClause(clause)
+
+    return KB, query
+
+# No changes needed in the writeOutput function
+def writeOutput(result, check, filename):
+    with open(filename, 'w') as f:
+        for loop_res in result:
+            f.write(str(len(loop_res)) + '\n')
+            for clause in loop_res:
+                string = ''
+                for c in clause:
+                    string += c
+                    if c != clause[-1]:
+                        string += ' OR '
+                f.write(string + '\n')
+        if check:
+            f.write('YES')
+        else:
+            f.write('NO')
+# Get list of input files in the current directory
+inputs = os.listdir(INPUT_DIR)
+
+for filename in inputs:
+    # Read KB and query from input file
+    KB, query = readKB(INPUT_DIR + filename)
+    # Perform PL resolution and get result
+    result, check = KB.PL_Resolution(query)
+    # Write output to a file in the same directory
+    writeOutput(result, check, OUTPUT_DIR + 'out-' + filename)
--- source/solution.py
+++ source/solution.py
@@ -23,13 +23,13 @@ class KnowledgeBase:
             self.clauses.append(clause)
     
     #Phủ định nguyên cái hàng chờ
-    def NegativeQuery(self, query):
+    def NegativeList(self, list):
         res = []
-        for clause in query:
-            new_query = []
-            for atom in new_query:
-                new_query.append(self.getNegative(atom))
-            res.append(new_query)
+        for clause in list:
+            new_list = []
+            for atom in list:
+                new_list.append(self.getNegative(atom))
+            res.append(new_list)
         return res
     
 
@@ -79,8 +79,54 @@ class KnowledgeBase:
                 for rest in self.cartesian_product(lists[1:]):
                     yield [item] + rest
 
-    
-    
+    def resolve(self, clause_i, clause_j):
+        new_clauses = []
+        for atom in clause_i:
+            neg_atom = self.getNegative_atom(atom)
+            if neg_atom in clause_j:
+                new_clause = self.resolve_single(atom, neg_atom, clause_i, clause_j)
+                if new_clause:
+                    new_clauses.append(new_clause)
+        return new_clauses
+
+    def resolve_single(self, atom_i, atom_j, clause_i, clause_j):
+        temp_c_i = clause_i.copy()
+        temp_c_j = clause_j.copy()
+        temp_c_i.remove(atom_i)
+        temp_c_j.remove(atom_j)
+        if not temp_c_i and not temp_c_j:
+            return ['{}']
+        else:
+            merged_clause = temp_c_i + temp_c_j
+            merged_clause = self.normClause(merged_clause)
+            if not self.checkComplementary(merged_clause) and merged_clause not in self.clauses:
+                return merged_clause
+        return None
+
+    def PL_Resolution(self, query):
+        tempKB = KnowledgeBase()
+        tempKB.clauses = self.clauses.copy()
+
+        neg_query = self.getNegative_query(query)
+        for neg_atom in neg_query:
+            tempKB.addClause(neg_atom)
+
+        while True:
+            new_clauses = []
+            for i in range(len(tempKB.clauses)):
+                for j in range(i + 1, len(tempKB.clauses)):
+                    resolvents = tempKB.resolve(tempKB.clauses[i], tempKB.clauses[j])
+                    for res in resolvents:
+                        if res not in new_clauses:
+                            new_clauses.append(res)
+
+            if not new_clauses:
+                return [], False
+            elif ['{}'] in new_clauses:
+                return [], True
+
+            for clause in new_clauses:
+                tempKB.addClause(clause)
     
 
         
